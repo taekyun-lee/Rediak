c2765043093991624c7815a4f3ec3f19cf4a0f14
Taekyun Lee <taekyun.lee.94@gmail.com>
2019-07-12 15:31:49 +0900

    progress snapshot
    progress snapshot

--- snapshot.go
+++ snapshot.go
@@ -1,9 +1,12 @@
 package KangDB
 
 import (
+	"bytes"
+	"io/ioutil"
 	"sync"
 	"time"
-	"lz"
+	"github.com/klauspost/compress/zstd"
+	"encoding/gob"
 )
 
 type snapshot interface{
@@ -26,5 +29,28 @@ type ZippedSnapshot struct{
 
 	isCompressed bool
 	compressAlgorithm string
+}
+
+func (z *ZippedSnapshot) SaveSnapshot(path string, d interface{})error{
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(d)
+	if err != nil{
+		return err
+	}
+	w,err := zstd.NewWriter(nil)
+	if err != nil{
+		return err
+	}
+	src := buf.Bytes()
+	res := w.EncodeAll(src,make([]byte,0,len(src)))
+
+	err = ioutil.WriteFile("./tmp/tmpfile", res, 0644)
+	if err != nil{
+		return err
+	}
+	return nil
+
+
 }
 
